b444a031919c99dffbd2c86b4128faaefc5bd12b
mnmoawiye <masoudmnm21@gmail.com>
2026-04-23 08:48:05 -0400

    added value error for same car_id

--- test_garage.py
+++ test_garage.py
@@ -1,9 +1,15 @@
+import pytest
 from garage import enter_garage, get_available_spots
 
 def test_enter_garage():
     garage = {"capacity": 2, "cars": {}}
     enter_garage(garage, "A", 5)
     assert garage["cars"]["A"] == 5
+
+def test_enter_same_car_id():
+    garage = {"capacity": 2, "cars": {"A": 1}}
+    with pytest.raises(ValueError):
+        enter_garage(garage, "A", 5)
 
 def test_available_spots_empty():
     garage = {"capacity": 10, "cars": {}}
