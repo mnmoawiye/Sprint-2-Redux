59e4158b084195e18a057cc86f079ef98a48fe62
mnmoawiye <masoudmnm21@gmail.com>
2026-04-23 08:21:17 -0400

    skipped to test_available_spots

--- test_garage.py
+++ test_garage.py
@@ -1,6 +1,10 @@
-from garage import enter_garage
+from garage import enter_garage, get_available_spots
 
 def test_enter_garage():
     garage = {"capacity": 2, "cars": {}}
     enter_garage(garage, "A", 5)
     assert garage["cars"]["A"] == 5
+
+def test_available_spots_empty():
+    garage = {"capacity": 10, "cars": {}}
+    assert get_available_spots(garage) == 10
