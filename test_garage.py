e63e4cf76329a2ac38f234558111a844dfb2a854
mnmoawiye <masoudmnm21@gmail.com>
2026-04-23 08:09:47 -0400

    fixed error

--- test_garage.py
+++ test_garage.py
@@ -3,4 +3,4 @@ from garage import enter_garage
 def test_enter_garage():
     garage = {"capacity": 2, "cars": {}}
     enter_garage(garage, "A", 5)
-    assert g["cars"]["A"] == 5
+    assert garage["cars"]["A"] == 5
