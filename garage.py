2679977a103372179cee889d4b19f983930c0ca7
mnmoawiye <masoudmnm21@gmail.com>
2026-04-24 07:51:29 -0400

    wrote code for exit garage

--- garage.py
+++ garage.py
@@ -8,6 +8,8 @@ def enter_garage(garage, car_id, entry_hour):
 
 
 def exit_garage(garage, car_id):
+    if car_id not in garage["cars"]:
+        raise KeyError
     del garage["cars"][car_id]
 
 def get_available_spots(garage):
